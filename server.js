11168f39dff5181e4bb702f3b61b19198a85ab29
stefanoricic <orcic.stefan@gmail.com>
2019-11-16 15:51:04 +0100

    heroku fix

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/mbr'));
+app.use(express.static(__dirname + '/dist/maticni'));
 app.listen(process.env.PORT || 8080);
 
 //PATH LOCATION STARTEGY
 
 app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/mbr/index.html');
+  const fullPath = path.join(__dirname + '/dist/maticni/index.html');
   console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
 })
